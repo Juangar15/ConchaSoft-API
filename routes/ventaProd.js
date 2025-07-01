b79393630ec671a65ca6ec2150e891b61a4d17cb
juan <garcesjuan1505@gmail.com>
2025-07-01 18:38:04 -0500

    solucionando error ventaProd

--- routes/ventaProd.js
+++ routes/ventaProd.js
@@ -9,13 +9,4 @@ router.get('/', verificarToken, verificarPermiso('ver_ventaProd'), ventaProdCont
 // Obtener una relación específica por id_venta e id_producto_talla
 router.get('/:id_venta/:id_producto_talla', verificarToken, verificarPermiso('ver_ventaProd'), ventaProdController.obtenerVentaProducto);
 
-// Crear una nueva relación venta-producto
-router.post('/', verificarToken, verificarPermiso('crear_ventaProd'), ventaProdController.crearVentaProducto);
-
-// Actualizar una relación venta-producto
-router.put('/:id_venta/:id_producto_talla', verificarToken, verificarPermiso('editar_ventaProd'), ventaProdController.actualizarVentaProducto);
-
-// Eliminar una relación venta-producto
-router.delete('/:id_venta/:id_producto_talla', verificarToken, verificarPermiso('eliminar_ventaProd'), ventaProdController.eliminarVentaProducto);
-
 module.exports = router;
